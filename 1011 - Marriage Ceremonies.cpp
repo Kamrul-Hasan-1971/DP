32a1e1458cc62f8a487a1c22764483eec0046a0d
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-05-21 16:31:00 +0600

    Update 1011 - Marriage Ceremonies.cpp

--- 1011 - Marriage Ceremonies.cpp
+++ 1011 - Marriage Ceremonies.cpp
@@ -2,43 +2,26 @@
 using namespace std;
 int Set(int N,int pos){return N=N | (1<<pos);}
 bool check(int N,int pos){return (bool)(N & (1<<pos));}
-int reset(int N,int pos){return N= N & ~(1<<pos);}
-string s1 ;
-int n,dp[1<<13];
-int call(int mask,int indx)
+int priority[16][16],n,dp[16][1<<16];
+int call(int groom ,int mask)
 {
-    if(dp[mask]!=-1) return dp[mask];
-    int ret1=0;
-    for(int i=0; i<=10; i++)
-    {
-        if(check(mask,i)==1 && check(mask,i+1)==1)
-        {
-            if(i-1>=0 && check(mask,i-1)==0)
-                ret1 = max(ret1,1+call((Set(reset(reset(mask,i+1),i),i-1)),1));
-            if(i+2<12 && check(mask,i+2)==0)
-                ret1 = max(ret1,1+call((Set(reset(reset(mask,i+1),i),i+2)),0));
-        }
-    }
-    return dp[mask]=ret1;
+    if(mask==(1<<n)-1) return 0;
+    if(dp[groom][mask]!=-1) return dp[groom][mask];
+    int mx=0;
+    for(int i=0; i<n; i++) if(check(mask,i)==0) mx=max(mx,priority[groom][i]+call(groom+1,Set(mask,i)));
+    return dp[groom][mask]=mx;
 }
 int main()
 {
-    int T, i,j, k;
+    int T , i ,j , k;
     scanf("%d",&T);
     for( int k = 1 ; k <= T ; k++)
     {
-        int num = 0,cnt1=0;
         memset(dp,-1,sizeof dp);
-        cin >> s1 ;
-        for(i=0; i<12; i++)
-        {
-            if(s1[i]=='o')
-            {
-                num = Set(num,11-i);
-                cnt1++;
-            }
-        }
-        printf("%d\n",cnt1-call(num,1));
+        scanf("%d",&n);
+        for(i=0; i<n; i++) for(j=0; j<n; j++) scanf("%d",&priority[i][j]);
+        printf("Case %d: %d\n",k,call(0,0));
     }
     return 0;
 }
+//http://lightoj.com/volume_showproblem.php?problem=1011
