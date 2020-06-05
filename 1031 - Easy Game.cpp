c88595d0d10c86efe3f11212909badc2fffa48b7
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-06-05 20:00:10 +0600

    Update 1031 - Easy Game.cpp

--- 1031 - Easy Game.cpp
+++ 1031 - Easy Game.cpp
@@ -1,104 +1,44 @@
-#include <bits/stdc++.h>
-
-#define pii             pair <int,int>
-#define sc              scanf
-#define pf              printf
-#define Pi              2*acos(0.0)
-#define ms(a,b)         memset(a, b, sizeof(a))
-#define pb(a)           push_back(a)
-#define MP              make_pair
-#define db              double
-#define ll              long long
-#define EPS             10E-10
-#define ff              first
-#define ss              second
-#define sqr(x)          (x)*(x)
-#define D(x)            cout<<#x " = "<<(x)<<endl
-#define VI              vector <int>
-#define DBG             pf("Hi\n")
-#define MOD             100007
-#define MAX             10000
-#define CIN             ios_base::sync_with_stdio(0); cin.tie(0)
-#define SZ(a)           (int)a.size()
-#define sf(a)           scanf("%d",&a)
-#define sfl(a)          scanf("%lld",&a)
-#define sff(a,b)        scanf("%d %d",&a,&b)
-#define sffl(a,b)       scanf("%lld %lld",&a,&b)
-#define sfff(a,b,c)     scanf("%d %d %d",&a,&b,&c)
-#define sfffl(a,b,c)    scanf("%lld %lld %lld",&a,&b,&c)
-#define loop(i,n)       for(int i=0;i<n;i++)
-#define REP(i,a,b)      for(int i=a;i<b;i++)
-#define TEST_CASE(t)    for(int z=1;z<=t;z++)
-#define PRINT_CASE      printf("Case %d: ",z)
-#define CASE_PRINT      cout<<"Case "<<z<<": ";
-#define all(a)          a.begin(),a.end()
-#define intlim          2147483648
-#define inf             1000000
-#define ull             unsigned long long
-
+#include<bits/stdc++.h>
 using namespace std;
-
-
-/*----------------------Graph Moves----------------*/
-//const int fx[]={+1,-1,+0,+0};
-//const int fy[]={+0,+0,+1,-1};
-//const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
-//const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
-//const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
-//const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
-/*------------------------------------------------*/
-
-/*-----------------------Bitmask------------------*/
-//int Set(int N,int pos){return N=N | (1<<pos);}
-//int reset(int N,int pos){return N= N & ~(1<<pos);}
-//bool check(int N,int pos){return (bool)(N & (1<<pos));}
-/*------------------------------------------------*/
-
-int dp[101][101];
-int ara[101];
-
-int call(int i, int j)
+ 
+#define  CIN            ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define  ll             long long int
+#define  mn             -100000000
+ll ara[102],dp[102][102],tc,vis[102][102];
+ll fun(ll i , ll j )
 {
-    if(i>j || j<i) return 0;
-    int &ret=dp[i][j];
-    if(ret!=-1) return ret;
-
-    int p=0,difference=-100000000;
-
-    for(int k=i;k<=j;k++)
+    if(i>j ) return 0 ;
+    ll &ret1 = dp[i][j];
+    ll &v = vis[i][j];
+    if(v==tc) return ret1 ;
+    v=tc;
+    ll sum = 0 , gap = mn;
+    for( ll k = i ; k <= j ; k++ )
     {
-        p+=ara[k];
-        difference=max(difference,p-call(k+1,j));
+        sum+=ara[k];
+        gap = max(gap,sum-fun(k+1,j));
     }
-
-    p=0;
-
-    for(int k=j;k>=i;k--)
+    sum = 0;
+    for( ll k = j ; k >= i ; k-- )
     {
-        p+=ara[k];
-        difference=max(difference,p-call(i,k-1));
+        sum+=ara[k];
+        gap = max(gap,sum-fun(i,k-1));
     }
-    return ret=difference;
+    return ret1=gap ;
+ 
 }
-
 int main()
 {
-    ///freopen("in.txt","r",stdin);
-    ///freopen("out.txt","w",stdout);
     CIN;
-    int t;
-    cin>>t;
-    TEST_CASE(t)
+    ll a,i,T;
+    cin>> T ;
+    while(T--)
     {
-        int n;
-        cin>>n;
-        loop(i,n) cin>>ara[i];
-        ms(dp,-1);
-
-        CASE_PRINT;
-        cout<<call(0,n-1)<<endl;
-
+        tc++;
+        cin >> a ;
+        for( i= 1 ; i <= a ; i++) cin >> ara[i];
+        ll ans = fun(1,a);
+        printf("Case %lld: %lld\n",tc,ans);
     }
-    return 0;
+    return 0 ;
 }
-//http://lightoj.com/volume_showproblem.php?problem=1031
